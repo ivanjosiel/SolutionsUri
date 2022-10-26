5f2bd5f18c90d1db2bd2eeb6f77f2420e6f46950
Jackson Martins <33990342+wjacksson@users.noreply.github.com>
2022-10-26 09:19:57 -0300

    Create URI - 1016.C

---
+++ URI - 1016.C
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	int X=60, Y=90,d;
+	scanf("%d",&d);
+	
+	
+	X=(60*d)/30;
+	
+	printf("%d minutos\n",X);
+	
+	system ("pause");
+	return 0;
+}
